8ab82d686110e48f1b0fc380ccb8a18cd74e5c3f
truongcon17 <aduvip1111111111@gmail.com>
2024-12-12 15:32:41 +0700

    Add files via upload

---
+++ Ex3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "Hello, World!";
+
+    int len = strlen(str);
+    
+    printf("Chuoi dao nguoc: ");
+    for (int i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+    
+    printf("\n"); 
+    return 0;
+}
+
